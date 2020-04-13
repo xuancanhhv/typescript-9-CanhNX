48e02af1a6063c5eebe44a1c24d6aa66906b8b76
xuancanhhv <comprocoding@gmail.com>
2020-04-13 21:06:06 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-9vqpze
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-9vqpze)
---
+++ index.ts
@@ -0,0 +1,14 @@
+// Write TypeScript code!
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+//const appDiv: HTMLElement = document.getElementById('app');
+//appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+var employees = ['A', 'B', 'C', 'D'];
+let[emp1, emp2, emp3,emp4] = employees;
+console.log(emp1);
+console.log(emp2);
+console.log(emp3);
+console.log(emp4);
