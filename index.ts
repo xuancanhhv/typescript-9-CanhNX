6e79ae89696a2a9d384d9153e043b6ed23840287
xuancanhhv <comprocoding@gmail.com>
2020-04-28 18:15:04 +0700

    add file class

--- index.ts
+++ index.ts
@@ -1,16 +1,29 @@
 class Animal {
-  move(distanceInMeters: number = 0) {
-    console.log(`Animal moved ${distanceInMeters}m.`);
+  name: string; 
+  constructor(theName:string){this.name = this.theName;}
+  move(distanceInMeters:number= 0){
+    console.log(`${this.name} moved ${distanceInMeters}m`)
   }
 }
-
-class Dog extends Animal {
-  bark() {
-    console.log("Woof! Woof!");
-  }
+class Snake extends Animal {
+    constructor(name: string) { super(name); }
+    move(distanceInMeters = 5) {
+        console.log("Slithering...");
+        super.move(distanceInMeters);
+    }
 }
 
-const dog = new Dog();
-dog.bark();
-dog.move(10);
-dog.bark();
+class Horse extends Animal {
+    constructor(name: string) { super(name); }
+    move(distanceInMeters = 45) {
+        console.log("Galloping...");
+        super.move(distanceInMeters);
+    }
+}
+
+let sam = new Snake("Sammy the Python");
+let tom: Animal = new Horse("Tommy the Palomino");
+
+sam.move();
+tom.move(34);
+
